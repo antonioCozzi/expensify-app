8fac064930ff49cf5f855223e75c1290d9d47641
Antonio Cozzi <antoniocozzi2@gmail.com>
2017-12-24 11:31:44 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-     entry: './src/app.js',
-     output: {
-          path: path.join(__dirname, 'public'), //absolute path: the one for the pjt on your machine. Così facendo, diciamo di prendere la directory automaticamente e di mergiarla con la cartella public
-          filename: 'bundle.js'
-     },
-     module: {
-          rules: [{
-               loader: 'babel-loader',
-               test: /\.js$/, //ogni volta che vedi un qualcosa che finisce con .js, eseguilo con babel-loader
-               exclude: /node_modules/ //...tranne i node_modules (vengono esclusi dal compilatore)
+module.exports = (env) => {
+     const isProduction = env === 'production'
+     const CSSExtract = new ExtractTextPlugin('styles.css')
+
+     return {
+          entry: './src/app.js',
+          output: {
+               path: path.join(__dirname, 'public'), //absolute path: the one for the pjt on your machine. Così facendo, diciamo di prendere la directory automaticamente e di mergiarla con la cartella public
+               filename: 'bundle.js'
           },
-          {
-               test: /\.s?css$/,
-               use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-               ]
-          }]
-     },
-     devtool: 'cheap-eval-source-map',//reindirizza, se ci sono errori in console, al componente dove è l'errore
-     devServer: {
-          contentBase: path.join(__dirname, 'public'),
-          historyApiFallback: true
+          module: {
+               rules: [{
+                    loader: 'babel-loader',
+                    test: /\.js$/, //ogni volta che vedi un qualcosa che finisce con .js, eseguilo con babel-loader
+                    exclude: /node_modules/ //...tranne i node_modules (vengono esclusi dal compilatore)
+               },
+               {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                         use: [
+                              'css-loader',
+                              'sass-loader'
+                         ]
+                    })
+               }]
+          },
+          plugins: [
+               CSSExtract
+          ],
+          devtool: isProduction ? 'source-map' : 'inline-source-map',//reindirizza, se ci sono errori in console, al componente dove è l'errore
+          devServer: {
+               contentBase: path.join(__dirname, 'public'),
+               historyApiFallback: true
+          }
      }
 }
