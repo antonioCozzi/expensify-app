e8f7b7420696f73b6be554eb6282b02c80781cc0
Antonio Cozzi <antoniocozzi2@gmail.com>
2018-02-01 14:50:50 +0100

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,8 +12,9 @@ const config = {
 firebase.initializeApp(config)
 
 const database = firebase.database()
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
-export { firebase, database as default }
+export { firebase, googleAuthProvider, database as default }
 
 // // child_removed -> event di firebase che fa una cosa ogni volta che un elemento viene rimosso
 // database.ref('expenses').on('child_removed', (snapshot) => {
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,13 @@
 import ReactShallowRenderer from 'react-test-renderer/shallow'
 import React from 'react'
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 import { shallow } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 
 // react-test-renderer
 test('should render Header correctly', () => {
-     const wrapper = shallow(<Header />)
-     expect(toJSON(wrapper)).toMatchSnapshot();
+     const wrapper = shallow(<Header startLogout={() => { }} />)
+     expect((wrapper)).toMatchSnapshot();
 
      // expect(wrapper.find('h1').text).toBe('Expensify')
 
@@ -23,3 +23,9 @@ test('should render Header correctly', () => {
      // console.log(renderer.getRenderOutput())
 })
 
+test('should call startLogout on button click', () => {
+     const startLogout = jest.fn()
+     const wrapper = shallow(<Header startLogout={startLogout} />)
+     wrapper.find('button').simulate('click')
+     expect(startLogout).toHaveBeenCalled()
+})
